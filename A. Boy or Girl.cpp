56d417512fdb97066d7dcb55e0be87463d1b8477
rishabh-397 <rishabh.cha2005@gmail.com>
2025-08-05 21:43:46 +0530

    A. Boy or Girl.cpp

---
+++ A. Boy or Girl.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    string s;
+    cin>>s;
+    vector<int>v(26,0);
+    for(auto c:s) v[c-'a']++;
+    int cnt=0;
+    for(auto c:v) if(c!=0) cnt++;
+    if(cnt%2){
+        cout<<"IGNORE HIM!";
+    
+    }else{
+        cout<<"CHAT WITH HER!";
+    }
+}
+ 
